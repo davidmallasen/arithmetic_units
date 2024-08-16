cc569128ff804687e7a9f63d832d1fbadfeec3ba
David Mallasen <dmallase@ucm.es>
2024-08-16 12:24:47 +0200

    Test again

--- hw/int/add/full_adder.sv
+++ hw/int/add/full_adder.sv
@@ -12,8 +12,8 @@
 // Delay: O(1)
 
 module full_adder (
-  input  logic  x,    // First operand
-  input  logic y,    // Second operand
+  input  logic x,    // First operand
+  input  logic  y,    // Second operand
   input  logic cin,  // Carry-in bit
   output logic s,    // Output sum
   output logic cout  // Carry-out bit
